223ab4f8600937fc3785159e50b6ee6e782ad52c
Danila Pechenev <danil-pechenev@mail.ru>
2022-03-31 20:02:25 +0300

    Initial commit of the second homework. Added Edge class.

---
+++ Term 2/Homework 2/SecondHomework/Kruskal/Edge.cs
@@ -0,0 +1,17 @@
+﻿namespace Kruskal;
+
+public class Edge
+{
+    public int FirstVertex { get; init; }
+
+    public int SecondVertex { get; init; }
+
+    public int Length { get; init; }
+
+    public Edge(int firstVertex, int secondVertex, int length)
+    {
+        FirstVertex = firstVertex;
+        SecondVertex = secondVertex;
+        Length = length;
+    }
+}
