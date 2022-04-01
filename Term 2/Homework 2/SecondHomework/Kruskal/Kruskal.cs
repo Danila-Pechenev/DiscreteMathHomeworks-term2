da49abb602c5d95ac322785786b7bbe427c5fc7c
Danila Pechenev <danil-pechenev@mail.ru>
2022-04-01 21:34:56 +0300

    Added Kruskal class.

---
+++ Term 2/Homework 2/SecondHomework/Kruskal/Kruskal.cs
@@ -0,0 +1,27 @@
+﻿namespace Kruskal;
+
+public static class Kruskal
+{
+    static List<Edge> FindMinSpanningTree(List<Edge> edges, int vertexCount)
+    {
+        edges.Sort((edge1, edge2) => edge1.Length.CompareTo(edge2.Length));
+        var DSU = new DisjointSetUnion(vertexCount);
+        var minSpanningTree = new List<Edge>();
+
+        for (int i = 0; i < edges.Count; i++)
+        {
+            Edge edge = edges[i];
+            if (DSU.FindSet(edge.FirstVertex) == DSU.FindSet(edge.FirstVertex))
+            {
+                continue;
+            }
+            else
+            {
+                minSpanningTree.Add(edge);
+                DSU.UnionSets(edge.FirstVertex, edge.SecondVertex);
+            }
+        }
+        
+        return minSpanningTree;
+    }
+}
