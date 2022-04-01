5361f1d9628ab7b819961e65d77a41be88cbdf0e
Danila Pechenev <danil-pechenev@mail.ru>
2022-04-01 21:35:27 +0300

    Added DisjointSetUnion.cs

---
+++ Term 2/Homework 2/SecondHomework/Kruskal/DisjointSetUnion.cs
@@ -0,0 +1,36 @@
+﻿namespace Kruskal;
+
+public class DisjointSetUnion
+{
+    private int[] parent;
+
+    public DisjointSetUnion(int n)
+    {
+        parent = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    public int FindSet(int vertex)
+    {
+        if (parent[vertex] == vertex)
+        {
+            return vertex;
+        }
+
+        return FindSet(parent[vertex]);
+    }
+
+    public void UnionSets(int vertex1, int vertex2)
+    {
+        int set1 = FindSet(vertex1);
+        int set2 = FindSet(vertex2);
+
+        if (set1 != set2)
+        {
+            parent[vertex1] = set1;
+        }
+    }
+}
