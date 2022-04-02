ce1aac1cd48a4091f8dd2eeac2f9aa3da5a95c46
Danila Pechenev <danil-pechenev@mail.ru>
2022-04-02 19:32:04 +0300

    Added EdgeTests.cs

---
+++ Term 2/Homework 2/SecondHomework/Kruskal.UnitTests/EdgeTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace Kruskal.UnitTests
+{
+    public class EdgeTests
+    {
+        [Test]
+        public void InitTest()
+        {
+            var edge = new Edge(3, 5, 12);
+            Assert.AreEqual(3, edge.FirstVertex);
+            Assert.AreEqual(5, edge.SecondVertex);
+            Assert.AreEqual(12, edge.Length);
+        }
+    }
+}
