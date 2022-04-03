585ceaa3dbbc49d2cbd3d6ff87909bb3adf6e9ee
Danila Pechenev <danil-pechenev@mail.ru>
2022-04-03 22:22:18 +0300

    Added unit tests for DSU.

---
+++ Term 2/Homework 2/SecondHomework/Kruskal.UnitTests/DSUTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Kruskal.UnitTests
+{
+    public class DSUTests
+    {
+        [Test]
+        public void FindSetAfterInitTest()
+        {
+            var dsu = new DisjointSetUnion(10);
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.AreEqual(i, dsu.FindSet(i));
+            }
+        }
+
+        [Test]
+        public void UnionSetsTest()
+        {
+            var dsu = new DisjointSetUnion(10);
+            for (int i = 0; i < 10 - 1; i++)
+            {
+                dsu.UnionSets(i, i + 1);
+            }
+
+            for (int i = 0; i < 10 - 1; i++)
+            {
+                Assert.AreEqual(dsu.FindSet(i), dsu.FindSet(i + 1));
+            }
+        }
+
+
+    }
+}
