b4cfa692f74e682c70b1f93f12f4daa4dbffef96
Danila Pechenev <danil-pechenev@mail.ru>
2022-04-03 22:23:04 +0300

    Added unit tests for Kruskal algorithm.

---
+++ Term 2/Homework 2/SecondHomework/Kruskal.UnitTests/KruskalTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Kruskal.UnitTests
+{
+    public class KruskalTests
+    {
+        [Test]
+        public void InitTest()
+        {
+            var edge = new Edge(3, 5, 12);
+            Assert.AreEqual(3, edge.FirstVertex);
+            Assert.AreEqual(5, edge.SecondVertex);
+            Assert.AreEqual(12, edge.Length);
+        }
+    }
+}
