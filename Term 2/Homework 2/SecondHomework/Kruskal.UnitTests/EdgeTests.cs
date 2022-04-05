15af9d55d0642a4f17e84c856c809e8f96d6142a
Danila Pechenev <danil-pechenev@mail.ru>
2022-04-05 11:13:10 +0300

    Formatting changes in EdgeTests.cs.

--- Term 2/Homework 2/SecondHomework/Kruskal.UnitTests/EdgeTests.cs
+++ Term 2/Homework 2/SecondHomework/Kruskal.UnitTests/EdgeTests.cs
@@ -1,16 +1,15 @@
 using NUnit.Framework;
 
-namespace Kruskal.UnitTests
+namespace Kruskal.UnitTests;
+
+public class EdgeTests
 {
-    public class EdgeTests
+    [Test]
+    public void InitTest()
     {
-        [Test]
-        public void InitTest()
-        {
-            var edge = new Edge(3, 5, 12);
-            Assert.AreEqual(3, edge.FirstVertex);
-            Assert.AreEqual(5, edge.SecondVertex);
-            Assert.AreEqual(12, edge.Length);
-        }
+        var edge = new Edge(3, 5, 12);
+        Assert.AreEqual(3, edge.FirstVertex);
+        Assert.AreEqual(5, edge.SecondVertex);
+        Assert.AreEqual(12, edge.Length);
     }
 }
