220992dcb52d72c2c1d3ed483594c57eb2f92b38
Danila Pechenev <danil-pechenev@mail.ru>
2022-05-03 21:10:44 +0300

    Add Dijkstra's algorithm

---
+++ Term 2/Homework 2/Algorithms/Dijkstra.cs
@@ -0,0 +1,64 @@
+﻿namespace Algoritms;
+using CommonClasses;
+
+public static class Dijkstra
+{
+    public static List<Edge> FindMinPath(int[,] matrix, int startVertex, int endVertex)
+    {
+        var path = new List<Edge>();
+
+        int N = matrix.GetLength(0);
+        int INF = (int)Math.Pow(10, 9);
+        var distances = new int[N];
+        var parents = new int[N];
+        for (int i = 1; i < N; i++)
+        {
+            distances[i] = INF;
+        }
+
+        var used = new bool[N];
+        int minDistance = 0;
+        int minVertex = startVertex;
+        while (minDistance < INF)
+        {
+            int i = minVertex;
+            used[i] = true;
+            for (int j = 0; j < N; j++)
+            {
+                if (distances[i] + matrix[i, j] < distances[j] && matrix[i, j] != 0)
+                {
+                    distances[j] = distances[i] + matrix[i, j];
+                    parents[j] = i;
+                }
+            }
+
+            minDistance = INF;
+            for (int j = 0; i < N; j++)
+            {
+                if (!used[j] && distances[j] < minDistance)
+                {
+                    minDistance = distances[j];
+                    minVertex = j;
+                }
+            }
+        }
+
+        int currentVertex = endVertex;
+        while (currentVertex != startVertex)
+        {
+            path.Add(new Edge(currentVertex, parents[currentVertex], matrix[currentVertex, parents[currentVertex]]));
+            currentVertex = parents[currentVertex];
+        }
+
+        path.Reverse();
+
+        return path;
+    }
+
+    public static List<Edge> FindMinPath(List<Edge> edges, int vertexCount, int startVertex, int endVertex)
+    {
+        var matrix = GraphRepresentation.EdgeListToAdjacencyMatrix(edges, vertexCount);
+
+        return FindMinPath(matrix, startVertex, endVertex);
+    }
+}
